8fe0fe0354c829aa0a57a65df2f88ff08c747b32
Kebinichi <haleca03@pfw.edu>
2025-11-24 15:34:16 -0500

    phaser implementation
    adding phaser tracking, some edits to the virtual cursor. Includes custom phaser inputs and our first flappy bird game.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ProfileSetup from "./pages/ProfileSetup.jsx";
 import Games from "./pages/Games.jsx";
 import GamePlay from "./pages/GamePlay.jsx";
 import Aboutpage from "./pages/Aboutpage.jsx";
+import CameraGame from "./pages/CameraGame.jsx";
 
 export default function App() {
   return (
@@ -30,6 +31,7 @@ export default function App() {
         <Route path="/games" element={<Games />} />
         <Route path="/play/:slug" element={<GamePlay />} />
         <Route path="/about" element={<Aboutpage />} />
+                <Route path="/camera-game" element={<CameraGame />} />
         <Route path="*" element={<div className="p-8">Page not found</div>} />
       </Routes>
     </>
--- src/lib/cursor/cursor.js
+++ src/lib/cursor/cursor.js
@@ -1,14 +1,8 @@
 //create the cursor and allow it to interact with the webpage
-import { getClickableElements } from "./utils.js";
 
 //initialize the cursor's position to be in the center of the screen
 let prevX = window.innerWidth / 2;
 let prevY = window.innerHeight / 2;
-
-//initialize hover tracking
-let hoverElement = null;
-let hoverStart = null;
-const HOVER_TIME = 1000;
 
 const cursor = document.getElementById("cursor");
 
@@ -34,36 +28,11 @@ export function onResultsHandler(results) {
 
 
   //make cursor movement smooth
-  prevX += (x - prevX) * 0.3;
-  prevY += (y - prevY) * 0.3;
+  prevX += (x - prevX) * 0.5;
+  prevY += (y - prevY) * 0.5;
 
   //update cursor position
   cursor.style.left = `${prevX}px`;
   cursor.style.top = `${prevY}px`;
 
-  //use util function to find all interactible(clickable) elements on the page
-  const clickableElements = getClickableElements();
-  let hovering = false;
-
-  //detect if cursor is hovering over an interactible element, and click after 1 second
-  clickableElements.forEach(el => {
-    const rect = el.getBoundingClientRect();
-    if (prevX > rect.left && prevX < rect.right && prevY > rect.top && prevY < rect.bottom) {
-      if (hoverElement !== el) {
-        hoverElement = el;
-        hoverStart = performance.now();
-      } else if (performance.now() - hoverStart >= HOVER_TIME) {
-        el.click();
-        hoverStart = performance.now();
-      }
-      hovering = true;
-    }
-  });
-
-
-  //if not hovering, reset hover tracking
-  if (!hovering) {
-    hoverElement = null;
-    hoverStart = null;
-  }
 }
