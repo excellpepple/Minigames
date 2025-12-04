1f1650fb869498e3a6ea65f5a30e1194ce6a97e1
Sanjana Gondariya <gondsd01@pfw.edu>
2025-12-04 16:31:49 -0500

    Update gameConfig.js

--- src/lib/phaser/games/gameConfig.js
+++ src/lib/phaser/games/gameConfig.js
@@ -23,10 +23,5 @@ export default function createGame(parentId) {
     transparent: true
   };
 
-<<<<<<< HEAD
   const game = new Phaser.Game(config);
   return game;
-=======
-  return new Phaser.Game(config);
->>>>>>> 94a84f30d7b1869b16bc4ae9606bdce50990e346
-}
